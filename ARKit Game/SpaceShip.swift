4d3e5f9a119d6b5170942ac57ac5577c2f39adff
Dinesh Kachhot <dinesh@betyourskills.com>
2017-07-04 09:45:54 +0530

    ARKit

---
+++ ARKit Game/SpaceShip.swift
@@ -0,0 +1,13 @@
+//
+//  SpaceShip.swift
+//  ARKit Game
+//
+//  Created by dinesh on 03/07/17.
+//  Copyright © 2017 AnjDenny. All rights reserved.
+//
+
+import UIKit
+
+class SpaceShip: SCNNode {
+
+}
